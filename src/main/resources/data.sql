7a03e8a899fa4267e80f41d4f2a436009b16d15b
Sanket Patil <Sanket.Patil@amdocs.com>
2024-03-13 00:26:14 +0530

    2nd-Commit::Providing initial data for user table using data.sql file and required configuration in application.properties for the same

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+------------------ USER TABLE -- start ------------------
+
+-- Inserting initial sample data in User table :
+INSERT INTO user(u_name, u_phone_no, u_role) VALUES('Sanket', 987654321, 'admin');
+INSERT INTO user(u_name, u_phone_no, u_role) VALUES('SPatil', 123456789, 'user');
+
+
+------------------ USER TABLE -- end ------------------
